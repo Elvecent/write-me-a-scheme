f1b3f4e2ea6317eb4fc93869710337132afa0112
Elvecent <kvalyavin@ya.ru>
2019-03-06 18:26:56 +0500

    Regroup tests

--- tests/test.hs
+++ tests/test.hs
@@ -3,13 +3,15 @@ module Main where
 import Test.Tasty
 import Test.Tasty.QuickCheck as QC
 
+import Scheme
+
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [parserProps]
+tests = testGroup "Tests" [printParse]
 
-parserProps :: TestTree
-parserProps = testGroup "Parser/Printer Properties" [printParse]
+printParse :: TestTree
+printParse = testGroup "Parser/Printer Properties" [ppQC]
 
-printParse = testGroup "QuickChecking..."
+ppQC = testGroup "QuickChecking Parser/Printer..."
   [ QC.testProperty "Dummy" (\x -> if (x :: Int) < 5 then True else False) ]
